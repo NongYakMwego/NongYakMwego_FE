4ed01322db9e12cbe2e81e9f646087fb8b3b034a
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-05-17 17:14:08 +0900

    crop페이지 완료(색, API 제외)

--- src/components/TextBalloon1.tsx
+++ src/components/TextBalloon1.tsx
@@ -1,7 +1,7 @@
 export default function TextBalloon1() {
   return (
     <svg
-      className="w-96 h-52"
+      className="w-96 h-52" // Tailwind로 실제 사이즈 조절
       viewBox="0 0 400 200"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -11,28 +11,37 @@ export default function TextBalloon1() {
             dx="0"
             dy="4"
             stdDeviation="6"
-            flood-color="rgba(0,0,0,0.1)"
+            floodColor="rgba(0,0,0,0.1)" // flood-color → floodColor
           />
         </filter>
       </defs>
 
+      {/* 말풍선 본체 */}
       <path
         d="
-      M50,40 
-      a30,30 0 0 1 30,-30 
-      h240 
-      a30,30 0 0 1 30,30 
-      v100 
-      a30,30 0 0 1 -30,30 
-      h-95 
-      l-15,25 
-      l-15,-25 
-      h-115 
-      a30,30 0 0 1 -30,-30 
-      z"
+          M50,40 
+          a30,30 0 0 1 30,-30 
+          h240 
+          a30,30 0 0 1 30,30 
+          v100 
+          a30,30 0 0 1 -30,30 
+          h-95 
+          l-15,25 
+          l-15,-25 
+          h-115 
+          a30,30 0 0 1 -30,-30 
+          z"
         fill="white"
         filter="url(#cuteShadow)"
       />
+
+      {/* foreignObject 안에 텍스트 넣기 */}
+      <foreignObject x="60" y="60" width="280" height="80">
+        <div className="w-full h-full flex flex-col justify-center items-center text-xl font-bold text-[#333] text-center ">
+          <div>현재 키우시는 농작물을</div>
+          <div>선택해주세요! 😄</div>
+        </div>
+      </foreignObject>
     </svg>
   );
 }
--- src/pages/select/crop.tsx
+++ src/pages/select/crop.tsx
@@ -1,11 +1,48 @@
 import TextBalloon1 from "../../components/textBalloon1";
 import characterLogo from "../../assets/characterLogo.svg";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Crop() {
   return (
-    <div className="flex flex-col items-center ">
-      <TextBalloon1 />
-      <img src={characterLogo} className="w-60 h-60" />
+    <div className="flex flex-col items-center py-5 px-10 h-full justify-between">
+      <div className="flex flex-col items-center">
+        <TextBalloon1 />
+        <img src={characterLogo} className="w-60 h-60" />
+      </div>
+      <form className="flex flex-col space-y-10 w-full">
+        <div className="flex flex-col space-y-5">
+          <div className="flex flex-col space-y-2 ">
+            <span className="font-semibold">직접 검색하기</span>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="농작물을 검색하세요"
+                className="border-none h-11 w-full rounded-full pl-4 pr-10"
+              />
+              <IoIosSearch className="absolute top-1/2 -translate-y-1/2 right-0 w-6 h-6 mr-3" />
+            </div>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <span className="font-semibold">직접 입력하기</span>
+            <input
+              type="text"
+              placeholder="농작물을 입력하세요"
+              className="border-none h-11 w-full rounded-full pl-4"
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <span className="font-semibold">직접 사진찍기</span>
+            <input
+              type="text"
+              placeholder="농작물을 찍으세요"
+              className="border-none h-11  w-full rounded-full pl-4"
+            />
+          </div>
+        </div>
+        <button className="bg-red-500 h-11 w-full rounded-full font-bold text-lg">
+          다음
+        </button>
+      </form>
     </div>
   );
 }
